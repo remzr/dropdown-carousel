75486b25ac65ad5cf6160eef825d5510bec683c3
remzr <110097561+remzr@users.noreply.github.com>
2025-11-25 22:15:14 +0100

    Finish dropdown function and styling

--- dropdown.js
+++ dropdown.js
@@ -1,11 +1,13 @@
 //Add event listener
-const firstDropdown = document.querySelector("#dropdown-1");
+const firstDropdown = document.querySelector(".dropdown-1");
+const firstDropdownElements = document.querySelectorAll(".dropdown-1");
 
-firstDropdown.addEventListener("click", (event) => {
-    const currentTarget = event.currentTarget.getAttribute("value");
-    const target = event.target.getAttribute("id");
-    console.log(currentTarget);
-    console.log(target);
+firstDropdown.addEventListener("click", () => {
+    
+    for (let i = 0; i < firstDropdownElements.length; i++) {
+        firstDropdownElements[i].classList.toggle("visible");
+    }
+
 })
 
 
