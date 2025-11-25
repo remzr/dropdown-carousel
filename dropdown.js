902d210db1cb378fd66fada34230684fa0fe60af
remzr <110097561+remzr@users.noreply.github.com>
2025-11-25 21:36:06 +0100

    Add event listener

--- dropdown.js
+++ dropdown.js
@@ -1,1 +1,18 @@
-console.log("This is dropdown.js");
+//Add event listener
+const firstDropdown = document.querySelector("#dropdown-1");
+
+firstDropdown.addEventListener("click", (event) => {
+    const currentTarget = event.currentTarget.getAttribute("value");
+    const target = event.target.getAttribute("id");
+    console.log(currentTarget);
+    console.log(target);
+})
+
+
+/*
+    if (target.data-dropdown-state == "closed") {
+
+    } else {
+
+    }
+}; */
